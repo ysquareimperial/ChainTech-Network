946360bbae3a8e72986b71318790f7c917ea8eae
ysquareimperial <ysquare.theimperial@gmail.com>
2024-03-29 13:43:56 +0100

    save

--- src/ServicesSection.jsx
+++ src/ServicesSection.jsx
@@ -4,6 +4,52 @@ import { Card, Col, Row } from "reactstrap";
 import logo from "../src/assets/logo.png";
 import cardImg from "../src/assets/womanI.png";
 function ServicesSection() {
+  const cardDetails = [
+    {
+      cardLogo: logo,
+      cardHeading: "Join Development",
+      cardText: "Blockchain Development",
+      cardImage: cardImg,
+      cardButton: (
+        <button>
+          Explore <IoIosArrowRoundForward className="card_btn_icon" />
+        </button>
+      ),
+    },
+    {
+      cardLogo: logo,
+      cardHeading: "Join Development",
+      cardText: "Blockchain Development",
+      cardImage: cardImg,
+      cardButton: (
+        <button>
+          Explore <IoIosArrowRoundForward className="card_btn_icon" />
+        </button>
+      ),
+    },
+    {
+      cardLogo: logo,
+      cardHeading: "Join Development",
+      cardText: "Blockchain Development",
+      cardImage: cardImg,
+      cardButton: (
+        <button>
+          Explore <IoIosArrowRoundForward className="card_btn_icon" />
+        </button>
+      ),
+    },
+    {
+      cardLogo: logo,
+      cardHeading: "Join Development",
+      cardText: "Blockchain Development",
+      cardImage: cardImg,
+      cardButton: (
+        <button>
+          Explore <IoIosArrowRoundForward className="card_btn_icon" />
+        </button>
+      ),
+    },
+  ];
   return (
     <Row className="m-0 pt-5 pb-5 services_sec">
       <Col md={2}></Col>
@@ -20,23 +66,29 @@ function ServicesSection() {
           </div>
         </div>
         <Row className="mt-2">
-          <Col md={3}>
-            <Card className="services_card card1">
-              <div className="card_header d-flex gap-2 align-items-center">
-                <img src={logo} alt="chaintech" className="card_logo" />
-                <p className="m-0">Join Development</p>
-              </div>
-              <div className="card_body d-flex flex-column align-items-center">
-                <p className="mt-4">Blockchain Development</p>
-                <img src={cardImg} alt="card_img" className="mt-4 card_img" />
-              </div>
-              <div className="card_footer">
-                <button>
-                  Explore <IoIosArrowRoundForward className="card_btn_icon" />
-                </button>
-              </div>
-            </Card>
-          </Col>
+          {cardDetails.map((item, index) => (
+            <Col md={3}>
+              <Card key={index} className="services_card card1">
+                <div className="card_header d-flex gap-2 align-items-center">
+                  <img
+                    src={item.cardLogo}
+                    alt="chaintech"
+                    className="card_logo"
+                  />
+                  <p className="m-0">{item.cardHeading}</p>
+                </div>
+                <div className="card_body d-flex flex-column align-items-center">
+                  <p className="mt-4">{item.cardText}</p>
+                  <img
+                    src={item.cardImage}
+                    alt="card_img"
+                    className="mt-4 card_img"
+                  />
+                </div>
+                <div className="card_footer">{item.cardButton}</div>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Col>
       <Col md={2}></Col>
